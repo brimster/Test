1bec151ce37f5c75a372c90ff1429f573a523f58
brimster <brimmy@gmail.com>
2011-05-17 22:15:06 -0700

    Edited test.pl via GitHub

--- test.pl
+++ test.pl
@@ -1,12 +1,4 @@
 #!/usr/bin/perl
-<<<<<<< HEAD
 
-print "It compiles!!!!@@#@  YES\n";
 
-=======
-<<<<<<< HEAD
-print "It compiles!!!!@@#@  YES\n";
-=======
-print "It compiles!!!!@@#@\n";
->>>>>>> e38e69e883626ce17338158e8bb6c810d399d34c
->>>>>>> cd0b60e158a82299398e596a98bcd7a8c69ce8bf
+print "It compiles!!!!@@#@   YES\n";
