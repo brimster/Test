9326e2352258e615b2a69b024e77f6b68a20fc4d
David Chan <brimmy@gmail.com>
2011-05-17 22:08:53 -0700

    lk
    Signed-off-by: David Chan <brimmy@gmail.com>

--- test.pl
+++ test.pl
@@ -1,6 +1,4 @@
 #!/usr/bin/perl
-<<<<<<< HEAD
-print "It compiles!!!!@@#@  YES\n";
-=======
-print "It compiles!!!!@@#@\n";
->>>>>>> e38e69e883626ce17338158e8bb6c810d399d34c
+
+print "It compiles!!!!@ @#@    YES\n";
+
